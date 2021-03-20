65bdd036f433c4a53c303bdd003de80f330f22f4
austinyen56 <38061641+austinyen56@users.noreply.github.com>
2021-03-20 10:30:29 -0700

    Added more servers and end message
    Added more servers so that users would only have to change the directory
    Added a end message before exiting

--- League Language Changer.bat
+++ League Language Changer.bat
@@ -1,13 +1,30 @@
 @echo off
-echo League of Legends Language Changer v1.0 by Austin56
-set /p comm=Enter server name(na/lan):
+echo League of Legends Language Changer v1.2 by Austin56
+set /p comm=Enter server name(na/lan/brazil/eune/euw/las/oce/russia/turkey/japan):
 echo All language options (input language in the parenthesis)  & echo.Czech Republic(czech) & echo.Greece(greek) & echo.Poland(polish) & echo.Romania(romanian) & echo.Hungary(hungarian) & echo.Germany(german) & echo.Spanish(spanish_spain) & echo.Italy(italian) & echo.France(french) & echo.Mexico(spanish_mexico) & echo.Brazil(portuguese) & echo.Malaysia(malay) & echo.Thailand(thai) & echo.Vietnam(vietnamese) & echo.Indonesia(indonesian) & echo.Malaysia(chinese_malaysia) & echo.Chinese(chinese_tw) & echo.Chinese(chinese_china) & echo.Japan(japanese) & echo.Korea(korean) & echo.English(english) & echo.Turkey(turkish) & echo.Russian(russian)
 set /p lang=What language do u wanna change it to:
 
+:: Change the directory (dir = "this thing") if you located the client in a different place
 if %comm%==na set dir="C:\League of Legends\LeagueClient.exe" 
 if %comm%==na set filename=Language Change NA
-if %comm%==lan set dir="D:\League of Legends LAN\LeagueClient.exe" 
+if %comm%==lan set dir="C:\League of Legends\LeagueClient.exe" 
 if %comm%==lan set filename=Language Change LAN
+if %comm%==brazil set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==brazil set filename=Language Change BR
+if %comm%==enue set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==eune set filename=Language Change EUNE
+if %comm%==euw set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==euw set filename=Language Change EUW
+if %comm%==las set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==las set filename=Language Change LAS
+if %comm%==oce set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==oce set filename=Language Change OCE
+if %comm%==russia set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==russia set filename=Language Change RU
+if %comm%==turkey set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==turkey set filename=Language Change TR
+if %comm%==japan set dir="C:\League of Legends\LeagueClient.exe" 
+if %comm%==japan set filename=Language Change JP
 
 if %lang%==czech set lang=cs_CZ
 if %lang%==greek set lang=el_GR
@@ -40,6 +57,8 @@ echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = %dir% >> %SCRIPT%
 echo oLink.Arguments = "--locale=%lang%" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
-
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+
+echo A new icon should appear on the desktop
+pause
